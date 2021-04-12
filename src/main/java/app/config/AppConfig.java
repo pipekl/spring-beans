1972597eb37ca014d8053276d2375aec6f6a8858
pipekl <79262323423@yandex.ru>
2021-04-12 12:46:31 +0300

    complete

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,5 +1,8 @@
 package app.config;
 
+import app.model.Cat;
+import app.model.Dog;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
